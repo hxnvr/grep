d0a4ff52a6d36a8b4886a05b7b54a10d0e888e02
hxnvr <hxnvr@mail.ru>
2019-03-29 21:17:11 +0300

    it works

--- src/grep/grep.kt
+++ src/grep/grep.kt
@@ -2,49 +2,49 @@ package grep
 
 import java.io.File
 
-class grep(line: String){
+class Grep(line: String){
     var v = false // инвертирует условие фильтрации
     var i = false // игнорировать регистр слов
     var r = false // вместо слова задаёт регулярное выражение для поиска
     private val lineSplit = line.split(" ")
     private val indexOfWord = lineSplit.lastIndex - 1
     private val indexOfFile = lineSplit.lastIndex
-    private val word = lineSplit[indexOfWord]
+    private val inputWord = lineSplit[indexOfWord]
     private val inputFile = lineSplit[indexOfFile]
 
     private fun wordGrep(lines: List<String>): List<String> {
-        val res = mutableListOf<String>()
+        val result = mutableListOf<String>()
         for (line in lines) {
-            var newWord = word
+            var newWord = inputWord
             var newLine = line
             if (i) {
-                newWord = word.toLowerCase()
+                newWord = newWord.toLowerCase()
                 newLine = line.toLowerCase()
             }
             if (!v) {
-                if (newLine.contains(newWord)) res.add(line)
-            } else if (!newLine.contains(newWord)) res.add(line)
+                if (newLine.contains(newWord)) result.add(line)
+            } else if (!newLine.contains(newWord)) result.add(line)
         }
-        return res
+        return result
     }
 
     private fun regexGrep(lines: List<String>): List<String> {
-        val res = mutableListOf<String>()
+        val result = mutableListOf<String>()
         for (line in lines) {
-            var newWord = word
+            var newWord = inputWord
             var newLine = line
             if (i) {
-                newWord = word.toLowerCase()
+                newWord = newWord.toLowerCase()
                 newLine = line.toLowerCase()
             }
             if (!v) {
-                if (newLine.contains(Regex(newWord))) res.add(line)
-            } else if (!Regex(newWord).matches(newLine.split(" ")[indexOfWord])) res.add(line)
+                if (newLine.contains(Regex(newWord))) result.add(line)
+            } else if (!newLine.contains(Regex(newWord))) result.add(line)
         }
-        return res
+        return result
     }
 
-    fun grepGrep(): List<String> {
+    fun findStrings(): List<String> {
         if (lineSplit.contains("-i")) i = true
         if (lineSplit.contains("-v")) v = true
         if (lineSplit.contains("-r")) r = true
--- src/grep/main.kt
+++ src/grep/main.kt
@@ -2,8 +2,8 @@ package grep
 
 fun main(line: Array<String>) {
     val inputLine = readLine()!!.toString()
-    val grepp = grep(inputLine)
-    val strings = grepp.grepGrep()
+    val testGrep = Grep(inputLine)
+    val strings = testGrep.findStrings()
     for (string in strings){
         println(string)
     }
